ac6f5dbbdc30dd91ef859cfb62483e3811b41294
Quoc Tri Do <qdo@2-F-9.42heilbronn.de>
2024-04-27 00:13:17 +0200

    rest: algorithm to let phis take fork of other groups

--- philo/ft_create.c
+++ philo/ft_create.c
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:36:27 by qdo               #+#    #+#             */
-/*   Updated: 2024/04/26 23:00:02 by qdo              ###   ########.fr       */
+/*   Updated: 2024/04/27 00:09:43 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,23 @@
 static void	ft_philo_job_alone(void *param)
 {
 	t_philo	*philo;
+	int		time_cnt;
 
 	philo = (t_philo *)param;
+	gettimeofday(&philo->start, NULL);
+	pthread_mutex_lock(&philo->psfork[1].mutex);
 	printf("0 1 took a fork\n");
-	usleep(philo->time_die);
-	printf("%d 1 die\n", philo->time_die);
+	while (1)
+	{
+		gettimeofday(&philo->end, NULL);
+		time_cnt = (philo->end.tv_usec - philo->start.tv_usec) / 1000;
+		if (time_cnt >= philo->time_die)
+		{
+			printf("%d 1 die\n", time_cnt);
+			pthread_mutex_unlock(&philo->psfork[1].mutex);
+			break ;
+		}
+	}
 }
 
 static void	ft_check_die(t_philo *philo, int philo_sum)
@@ -33,15 +45,14 @@ static void	ft_check_die(t_philo *philo, int philo_sum)
 	{
 		if (philo[i].die == 1)
 		{
-			j = 0;
 			pthread_mutex_lock(philo[0].mutex_print);
 			gettimeofday(&now, NULL);
 			begin = ft_print_out(NULL, NULL);
 			printf("%d %d %s", (now.tv_usec - begin->tv_usec) / 1000,
 				philo[i].nbr, "die\n");
+			j = 0;
 			while (++j <= philo_sum)
 				philo[j].die = 1;
-			free(begin);
 			pthread_mutex_unlock(philo[0].mutex_print);
 			break ;
 		}
@@ -53,43 +64,36 @@ static void	ft_check_die(t_philo *philo, int philo_sum)
 void	ft_philo_job(void *philo_data)
 {
 	t_philo	*philo_i;
-	double	time_cnt;
+	int		time_cnt;
 
-	philo_i = (t_philo *)philo_data;
-	gettimeofday(&philo_i->start, NULL);
-	if ((philo_i->group == 1 && philo_i->psfork[0].used == philo_i->group)
-		|| )
-	{
-		pthread_mutex_lock(&philo_i->psfork[philo_i->nbr].mutex);
-		ft_print_out(philo_i, "took left fork\n");
-		if (philo_i->nbr == philo_i->sum)
-			pthread_mutex_lock(&philo_i->psfork[1].mutex);
-		else
-			pthread_mutex_lock(&philo_i->psfork[philo_i->nbr + 1].mutex);
-		ft_print_out(philo_i, "took right fork\n");
-		ft_print_out(philo_i, "is eating\n");
-		usleep(philo_i->time_eat * 1000 / 2);
-		time_cnt = ft_time_count(philo_i);
-		while (time_cnt < philo_i->time_eat - 100)
-			usleep((philo_i->time_eat * 1000 - time_cnt) / 2);
-		if (ft_time_count(philo_i) < philo_i->time_eat)
-			time_cnt = 0;
-	}
+
 }
 
 void	ft_philo_create(t_philo *philo, pthread_t *philo_id)
 {
-	int	i;
+	int				i;
+	struct timeval	*begin;
 
 	if (philo[0].sum == 1)
+	{
 		pthread_create(&philo_id[1], NULL, &ft_philo_job_alone,
 			(void *)&philo[1]);
+		pthread_join(philo_id[1], NULL);
+	}
 	else
 	{
 		i = 0;
+		begin = ft_print_out(NULL, NULL);
+		if (begin == NULL)
+			return ;
+		ft_time_count(NULL, 0);
 		while (++i <= philo[0].sum)
 			pthread_create(&philo_id[i], NULL, &ft_philo_job,
 				(void *)&philo[i]);
 		ft_check_die(philo, philo[0].sum);
+		i = 0;
+		while (++i <= philo[0].nbr)
+			pthread_join(philo_id[i], NULL);
+		free(begin);
 	}
 }
--- philo/other.c
+++ philo/other.c
@@ -6,16 +6,31 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 22:36:55 by qdo               #+#    #+#             */
-/*   Updated: 2024/04/26 22:37:05 by qdo              ###   ########.fr       */
+/*   Updated: 2024/04/26 23:40:37 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philolib.h"
 
-int	ft_time_count(t_philo *philo_i)
+int	ft_time_count(t_philo *philo_i, int check)
 {
-	gettimeofday(&philo_i->end, NULL);
-	return ((philo_i->end.tv_usec - philo_i->start.tv_usec) / 1000);
+	static struct timeval	*begin;
+
+	if (philo_i == 0 && check == -1)
+	{
+		begin = ft_print_out(NULL, NULL);
+		return (0);
+	}
+	if (check == 1 && philo_i->ate_times == 0)
+	{
+		gettimeofday(&philo_i->end, NULL);
+		return ((philo_i->end.tv_usec - begin->tv_usec) / 1000);
+	}
+	else
+	{
+		gettimeofday(&philo_i->end, NULL);
+		return ((philo_i->end.tv_usec - philo_i->start.tv_usec) / 1000);
+	}
 }
 
 struct timeval	*ft_print_out(t_philo *philo_i, char *str)
@@ -31,12 +46,12 @@ struct timeval	*ft_print_out(t_philo *philo_i, char *str)
 		gettimeofday(begin, NULL);
 		return (begin);
 	}
-	if (str != 0)
+	else if (philo_i != 0 && str != 0)
 	{
 		pthread_mutex_lock(philo_i->mutex_print);
 		gettimeofday(&now, NULL);
 		if (philo_i->die == 0)
-			printf("%d %d %s", (now.tv_usec - begin->tv_usec) / 1000,
+			printf("%d %d %s\n", (now.tv_usec - begin->tv_usec) / 1000,
 				philo_i->nbr, str);
 		pthread_mutex_unlock(philo_i->mutex_print);
 	}
